5d5d2274cf852f81f10918612fc96d357b902983
Harsh kumar <hk8994854@gmail.com>
2024-09-17 10:05:49 +0530

    Add files via upload

---
+++ circumference_of_circle.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float radius = 10;
+    float circumference;
+    circumference = 2*3.14*radius;
+
+    cout<<"The circumference of the circle of radius "<<radius<<"is "<<circumference<<endl;
+}
