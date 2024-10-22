e38f50a16f69826b8b21a9266937f1c07d7daeb4
Harsh kumar <hk8994854@gmail.com>
2024-10-22 15:34:58 +0530

    Add files via upload

---
+++ 1-10_count.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i=1;
+    while (i<11){
+        cout<<i<<endl;
+        i++;
+    }
+    cout<<""<<endl;
+    for(int i=1;i<11;i++){
+        cout<<i<<endl;
+    }
+}
