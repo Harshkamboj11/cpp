2a81986d1f9c6f7bf7879b2766caade2a2c52e15
Harsh kumar <hk8994854@gmail.com>
2024-11-07 21:36:02 +0530

    Add files via upload

---
+++ pattern_a6.cpp
@@ -0,0 +1,20 @@
+// NUMBER PYRAMID PATTERN
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+
+    cout<<"Enter the number of rows u need: ";
+    cin>>n;
+
+    for (int i=1;i<=n;i++){
+        for (int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        for (int k=1;k<=i;k++){
+            cout<<k<<" ";
+        }
+        cout<<endl;
+    }
+}
