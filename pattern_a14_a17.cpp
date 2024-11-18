bf9cefafb02a93dfda8239238f5fbc53b279ebbc
Harsh kumar <hk8994854@gmail.com>
2024-11-18 19:14:00 +0530

    Add files via upload

---
+++ pattern_a14_a17.cpp
@@ -0,0 +1,17 @@
+// Alphabet Triangle
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+
+    cout<<"Enter the number of rows u need: ";
+    cin>>n;
+
+    for (int i=0;i<=n;i++){
+        for (int j=0;j<i;j++){
+            cout<<char('A'+j);
+        }
+        cout<<endl;
+    }
+}
