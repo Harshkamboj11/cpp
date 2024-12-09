d22e84e88309d93454b6fed1469632936dc5fe54
Harsh kumar <hk8994854@gmail.com>
2024-12-09 16:26:09 +0530

    Add files via upload

---
+++ P1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+
+    cout<<"Enter the size of sqaure: ";
+    cin>>a;
+
+    int area = a*a;
+
+    cout<<"Area of the square is "<<area;
+}
